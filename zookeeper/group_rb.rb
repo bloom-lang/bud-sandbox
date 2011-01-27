7f0658dd431caf75e3f7b356aaf883b0a5d828fd
Neil Conway <nrc@cs.berkeley.edu>
2011-01-26 23:56:20 -0800

    Checkpoint.

--- zookeeper/group_rb.rb
+++ zookeeper/group_rb.rb
@@ -4,12 +4,30 @@ require "zookeeper"
 GROUP_NAME = "foo"
 ZK_ADDR = "localhost:2181"
 
-z = Zookeeper.new(ZK_ADDR)
-r = z.create(:path => "/bud")
-case r[:rc]
-  when Zookeeper::ZOK then puts "Created '/bud' successfully"
-  when Zookeeper::ZNODEEXISTS then puts "Path '/bud' exists already"
-  else puts "Unknown error: #{r[:rc]}"
+def mk_znode(z, path, ephemeral = false, sequence = false)
+  r = z.create(:path => path, :ephemeral => ephemeral, :sequence => sequence)
+  new_path = r[:path] ? r[:path] : path
+  case r[:rc]
+  when Zookeeper::ZOK then puts "Created '#{new_path}' successfully"
+  when Zookeeper::ZNODEEXISTS then puts "Path '#{new_path}' exists already"
+  else raise "Unknown error: #{r[:rc]}"
+  end
 end
 
+z = Zookeeper.new(ZK_ADDR)
+z.reopen
+mk_znode(z, "/bud")
+mk_znode(z, "/bud/#{GROUP_NAME}")
+mk_znode(z, "/bud/#{GROUP_NAME}/member", true, true)
+
+cb = Zookeeper::WatcherCallback.new {
+  puts "Callback!"
+  puts "Path: #{cb.context[:path]}"
+}
+
+z.get_children(:path => "/bud/#{GROUP_NAME}", :watcher => cb,
+               :watcher_context => { :path => "foo" })
+
+sleep 20
+puts "Done!"
 z.close
