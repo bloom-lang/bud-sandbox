346c10b8cb93e90848c8989ad310f574a0d1ba9a
Peter Alvaro <peteralvaro@shade.(none)>
2011-01-30 21:33:30 -0800

    multicast tweak

--- test/tc_multicast.rb
+++ test/tc_multicast.rb
@@ -33,9 +33,9 @@ end
 
 class TestMC < Test::Unit::TestCase
   def ntest_be
-    mc = MC.new("localhost", 34256, {'dump' => true})
-    mc2 = MC.new("localhost", 34257)
-    mc3 = MC.new("localhost", 34258)
+    mc = MC.new(:port => 34256, :dump => true)
+    mc2 = MC.new(:port =>  34257)
+    mc3 = MC.new(:port =>  34258)
 
     assert_nothing_raised(RuntimeError) { mc.run_bg; mc2.run_bg; mc3.run_bg } 
 
@@ -55,9 +55,9 @@ class TestMC < Test::Unit::TestCase
   end
 
   def test_rd
-    mc = RMC.new("localhost", 44256, {'dump' => true})
-    mc2 = RMC.new("localhost", 44257, {})
-    mc3 = RMC.new("localhost", 44258, {})
+    mc = RMC.new(:port => 44256, :dump => true)
+    mc2 = RMC.new(:port => 44257)
+    mc3 = RMC.new(:port => 44258)
 
     assert_nothing_raised(RuntimeError) { mc.run_bg; mc2.run_bg; mc3.run_bg } 
 
