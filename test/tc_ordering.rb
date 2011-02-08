6b1a50394f3abc5fd8e9c874c9d1d690ed412cc2
Peter Alvaro <peteralvaro@shade.(none)>
2011-02-08 11:00:33 -0800

    ordering test cleanup

--- test/tc_ordering.rb
+++ test/tc_ordering.rb
@@ -11,7 +11,6 @@ class ST < Bud
   
   def state
     super
-    periodic :tic, 1    
     table :mems, ['reqid', 'ident', 'payload']
   end 
 
@@ -27,18 +26,16 @@ end
 
 class SN < Bud
   include SimpleNonce
-  include TimeMoves
 end
 
 class GN < Bud
   include GroupNonce
   include StaticMembership
-  include TimeMoves
 end
 
 class TestSer < Test::Unit::TestCase
 
-  def test_group_nonce
+  def ntest_group_nonce
     gn = GN.new(:visualize => 3, :dump => true)
     gn.my_id <+ [[1]]
     gn.add_member <+ [['foo', 1]]
@@ -50,29 +47,29 @@ class TestSer < Test::Unit::TestCase
 
     (0..7).each do |t|
       sleep 1
-      puts "NON: #{gn.nonce.first.inspect}"
+      gn.sync_do{ puts "NON: #{gn.nonce.first.inspect}" } 
       #gn.member.each{ |m| puts "EM: #{m.inspect}" }
     end
   end
 
-  def nntest_assn
+  def ntest_assn
     as = AS.new
     as.run_bg
 
     sleep 10
   end
   
-  def ntest_simple_nonce
+  def test_simple_nonce
     sn = SN.new(:dump => true, :port => 235235)
     sn.run_bg
 
     old_val = 0
     (0..10).each do |i|
-      sn.nonce.each {|n| puts "NON: #{n}" } 
-      val = sn.nonce.first.ident
-      assert(val != old_val, "nonce had same value: #{old_val} (on run #{i})")
-      old_val = val
-      sleep 2
+      sn.sync_do { 
+        val = sn.nonce.first.ident
+        assert(val != old_val, "nonce had same value: #{old_val} (on run #{i})")
+        old_val = val
+      }
       # doesn't work.
       #sn.localtick <~ [['1234']]
     end
@@ -83,26 +80,33 @@ class TestSer < Test::Unit::TestCase
     as.run_bg
   end
 
-  def ntest_serialization
-    st = ST.new
+  def test_serialization
+    st = ST.new(:visualize => 3)
     st.run_bg
 
-    st.enqueue <+ [[1, 'foo']]
-    st.enqueue <+ [[2, 'bar']]
-    st.enqueue <+ [[3, 'baz']]
-    st.dequeue <+ [[1234]]
-    sleep 3
+    st.async_do{ st.enqueue <+ [[1, 'foo']] } 
+    st.async_do{ st.enqueue <+ [[2, 'bar']] }
+    s.async_do{ st.enqueue <+ [[3, 'baz']] }
+    st.sync_do{ }
+    st.sync_do{ 
+      st.dequeue <+ [[1234]] 
+    }
+    st.sync_do{ }
+    st.sync_do {
+      assert_equal(1, st.mems.length) 
+      assert_equal([1234, 1, 'foo'], st.mems.first)
+    }
 
-    assert_equal(1, st.mems.length)
-    assert_equal([1234, 1, 'foo'], st.mems.first)
-    st.dequeue <+ [[2345]]
-    sleep 3
-    assert_equal(2, st.mems.length)
-    st.mems.each_with_index do |m, i|
-      case i
-        when 0 then assert_equal([1234, 1, 'foo'], m)
-        when 1 then assert_equal([2345, 2, 'bar'], m)
+    st.sync_do{ st.dequeue <+ [[2345]] }
+    st.sync_do{ }
+    st.sync_do {
+      assert_equal(2, st.mems.length)
+      st.mems.each_with_index do |m, i|
+        case i
+          when 0 then assert_equal([1234, 1, 'foo'], m)
+          when 1 then assert_equal([2345, 2, 'bar'], m)
+        end
       end
-    end
+    }
   end
 end
