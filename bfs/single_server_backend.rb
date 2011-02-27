530cdcc0f42853d3dc098ea2829e19150a52e063
Peter Alvaro <peteralvaro@pangloss.(none)>
2011-02-26 18:20:11 -0800

    bfs cleanup

--- bfs/single_server_backend.rb
+++ bfs/single_server_backend.rb
@@ -13,20 +13,5 @@ class Servy
   include StaticMembership  
 end
 
-class DN
-  include Bud
-  include BFSDatanode
-end
-
-
-#dn = DN.new(23456, {})
-#dn.add_member <+ [["localhost:12345", 1]]
-#dn.run_bg
-
 s = Servy.new(:port => 12345)
 s.run
-
-#while true do
-#  sleep 1
-#end
-
