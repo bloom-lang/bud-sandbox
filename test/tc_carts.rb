0abe87a26872af1039cca48715d8439c734ddd8d
Neil Conway <nrc@cs.berkeley.edu>
2011-03-20 23:18:02 -0700

    Tweak cart test case.

--- test/tc_carts.rb
+++ test/tc_carts.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'bud'
-require 'bud/rendezvous'
 require 'test/unit'
 require 'test/cart_workloads'
 require 'cart/disorderly_cart'
@@ -84,14 +83,13 @@ class TestCart < Test::Unit::TestCase
   end
 
   def cart_test(program)
-    #program = BCS.new(:port => 23765)
     addy = "#{program.ip}:#{program.port}"
     add_members(program, addy)
     program.run_bg
     run_cart(program)
     sleep 1
-    program.sync_do{}
-    program.sync_do{
+    program.sync_do
+    program.sync_do {
       assert_equal(1, program.memo.length)
       #program.memo.each {|m| puts "MEMO: #{m.inspect}" }
       assert_equal(4, program.memo.first.array.length)
