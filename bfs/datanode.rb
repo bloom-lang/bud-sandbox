534fb21f5e36e8738e508382a98edfd21cd45076
Peter Alvaro <peteralvaro@pangloss.(none)>
2011-03-29 22:08:50 -0700

    suppress datanode printfs

--- bfs/datanode.rb
+++ bfs/datanode.rb
@@ -32,7 +32,10 @@ module BFSDatanode
 
     to_payload <= join([dir_contents, nonce]).map do |c, n|
       unless server_knows.map{|s| s.file}.include? c.file
+        #puts "BCAST #{c.file}; server doesn't know" or [n.ident, c.file, c.time]
         [n.ident, c.file, c.time]
+      else
+        #puts "server knows about #{server_knows.length} files"
       end
     end
     # base case
