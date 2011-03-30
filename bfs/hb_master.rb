78290b3ee758208af37b9da825f1141a3a409eed
Peter Alvaro <peteralvaro@pangloss.(none)>
2011-03-29 23:01:39 -0700

    bfs tweaks

--- bfs/hb_master.rb
+++ bfs/hb_master.rb
@@ -10,7 +10,10 @@ module HBMaster
 
   state do
     interface output, :available, [] => [:pref_list]
-    table :chunk_cache, [:node, :chunkid, :time]
+    #table :chunk_cache, [:node, :chunkid, :time]
+    table :chunk_cache, [:node, :chunkid] => [:time]
+    scratch :chunk_cache_alive, [:node, :chunkid, :time]
+
     scratch :chunk_cache_nodes, [:node]
     # at any given time, :available will contain a list of datanodes in preference order.
     # for now, arbitrary
@@ -19,23 +22,26 @@ module HBMaster
 
   bloom :hbmasterlogic do
     #stdio <~ last_heartbeat.inspected
-    chunk_cache <= join([master_duty_cycle, last_heartbeat]).flat_map do |d, l| 
-      unless l.payload[1].nil?
-        l.payload[1].map do |pay|
-          [l.peer, pay, Time.parse(d.val).to_f]
-        end 
-      end
+    chunk_cache <+ join([master_duty_cycle, last_heartbeat]).flat_map do |d, l| 
+      l.payload[1].map do |pay|
+        unless chunk_cache.map{|c| c.chunkid}.include? pay
+          [l.peer, pay, Time.parse(d.val).to_f] unless pay.nil?
+        end
+      end 
     end
+
+    chunk_cache_alive <= (chunk_cache * last_heartbeat).lefts
 
     hb_ack <~ last_heartbeat.map do |l|
       [l.sender, l.payload[0]] unless l.payload[1] == [nil]
     end
 
-    chunk_cache <- join([master_duty_cycle, chunk_cache]).map do |t, c|
-      c unless last_heartbeat.map{|h| h.peer}.include? c.node
-    end
+    #chunk_cache <- join([master_duty_cycle, chunk_cache]).map do |t, c|
+    #  c unless last_heartbeat.map{|h| h.peer}.include? c.node
+    #end
 
-    chunk_cache_nodes <= chunk_cache.map{ |cc| [cc.node] }
-    available <= chunk_cache_nodes.group(nil, accum(chunk_cache_nodes.node))
+    #chunk_cache_nodes <= chunk_cache.map{ |cc| [cc.node] }
+    available <= last_heartbeat.group(nil, accum(last_heartbeat.peer))
+    #available <= chunk_cache_nodes.group(nil, accum(chunk_cache_nodes.node))
   end
 end
