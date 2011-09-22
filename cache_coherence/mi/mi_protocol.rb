490ef4aefc2d2a7e45aa316cef6638e9be3c41a6
Neil Conway <nrc@cs.berkeley.edu>
2011-09-21 22:34:52 -0700

    Updates for BudCollection#schema change.

--- cache_coherence/mi/mi_protocol.rb
+++ cache_coherence/mi/mi_protocol.rb
@@ -5,14 +5,14 @@ module MIProtocol
     scratch :dc_template, [:@cache_id, :directory_id, :line_id]
 
     # channels shared by agents
-    channel :cdq_REX, cd_template.schema
+    channel :cdq_REX, cd_template.cols
     # really, want to make :payload functionally dependent on the other 3 cols.
-    channel :cdq_WBD, cd_template.schema.clone.push(:payload)
-    channel :dcp_REXD, dc_template.schema.clone.push(:payload)
-    channel :dcp_WBAK, dc_template.schema
-    channel :dcp_NAK, dc_template.schema
-    channel :dcq_INV, dc_template.schema
-    channel :cdp_INVD, cd_template.schema.clone.push(:payload)
+    channel :cdq_WBD, cd_template.cols.clone.push(:payload)
+    channel :dcp_REXD, dc_template.cols.clone.push(:payload)
+    channel :dcp_WBAK, dc_template.cols
+    channel :dcp_NAK, dc_template.cols
+    channel :dcq_INV, dc_template.cols
+    channel :cdp_INVD, cd_template.cols.clone.push(:payload)
 
     # EDB/a priori truth
     # necessary for bootstrapping states of lines, caches.
