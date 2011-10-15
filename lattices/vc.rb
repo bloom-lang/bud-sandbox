a3191d945c724636ee74ad564724439c9a21c126
Neil Conway <nrc@cs.berkeley.edu>
2011-10-14 17:02:01 -0700

    Cleanup VC example program.

--- lattices/vc.rb
+++ lattices/vc.rb
@@ -11,7 +11,6 @@ class VcAgent
     scratch :kickoff, [] => [:v]
     scratch :done, [] => [:v]
 
-    scratch :to_send, [:addr, :msg, :slow]
     channel :chn, [:@addr, :msg, :from, :clock]
     table :send_buf, [:addr, :msg, :send_at_time]
     scratch :buf_chosen, send_buf.schema
@@ -26,19 +25,18 @@ class VcAgent
   end
 
   bloom do
-    stdio <~ chn {|c| ["Got message @ #{port} (# = #{$addr_map[ip_port]}), ID = #{c.msg}, msg clock = #{c.clock.inspected(self)}"]}
+    stdio <~ chn {|c| ["Got message @ #{port} (node # = #{$addr_map[ip_port]}), msg ID = #{c.msg}, msg clock = #{c.clock.inspected}, local VC = #{my_vc.inspected}"]}
 
     # Setup a specific messaging scenario: node 3 will (usually) receive message
-    # 3 and then 1, violating causal order
-    to_send <= kickoff { [$nodes[2].ip_port, 1, true]}
-    to_send <= kickoff { [$nodes[1].ip_port, 2, false]}
-    to_send <= chn {|c| [$nodes[2].ip_port, 3, false] if $addr_map[ip_port] == 1}
-    done <= chn {|c| [true] if ($addr_map[ip_port] == 2 and c.msg == 1)}
+    # 3 followed by message 1, violating causal order
+    send_buf <= kickoff { [$nodes[2].ip_port, 1, @budtime + 2]}
+    send_buf <= kickoff { [$nodes[1].ip_port, 2, @budtime]}
+    send_buf <= chn {|c| [$nodes[2].ip_port, 3, @budtime] if $addr_map[ip_port] == 1}
 
-    send_buf <= to_send {|s| [s.addr, s.msg, s.slow ? (@budtime + 2) : @budtime]}
     buf_chosen <= send_buf {|s| s if s.send_at_time == @budtime}
     send_buf <- buf_chosen
     chn <~ buf_chosen {|s| [s.addr, s.msg, $addr_map[ip_port], next_vc]}
+    done <= chn {|c| [true] if ($addr_map[ip_port] == 2 and c.msg == 1)}
 
     # When we send or receive a message, bump the local VC; merge local VC with
     # VCs of incoming messages
@@ -73,7 +71,7 @@ q.pop
 
 $nodes.each do |n|
   n.sync_do {
-    puts n.my_vc.inspected(n).inspect
+    puts n.my_vc.inspected
   }
   n.stop
 end
