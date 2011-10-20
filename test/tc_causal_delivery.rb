2e6f17f1e07dcd669386dc4b2f68b5688150409a
Neil Conway <nrc@cs.berkeley.edu>
2011-10-20 13:41:11 -0700

    Tweak causal delivery test case.

--- test/tc_causal_delivery.rb
+++ test/tc_causal_delivery.rb
@@ -9,22 +9,37 @@ class CausalAgent
 end
 
 class TestCausalDelivery < Test::Unit::TestCase
+  def register_sent_cb(bud, q)
+    bud.register_callback(:pipe_sent) do
+      q.push(true)
+    end
+  end
+
   def test_basic
     agents = (1..3).map { CausalAgent.new }
     agents.each {|a| a.run_bg}
 
     a, b, c = agents
+    q_b = Queue.new
+    q_c = Queue.new
+    register_sent_cb(b, q_b)
+    register_sent_cb(c, q_c)
+
     a.sync_do {
       a.pipe_in <+ [[b.ip_port, a.ip_port, 1, "foo1"]]
     }
+    q_b.pop
+
     a.sync_do {
       a.pipe_in <+ [[b.ip_port, a.ip_port, 2, "foo2"]]
     }
+    q_b.pop
+
     a.sync_do {
       a.pipe_in <+ [[c.ip_port, a.ip_port, 3, "bar"]]
     }
+    q_c.pop
 
-    sleep 3
     agents.each {|a| a.stop}
   end
 end
