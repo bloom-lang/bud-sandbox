85083d4018c301872c9972bee8508a576cd79aad
Yoriyasu Yano <yoriy@captricity.com>
2011-11-02 13:29:57 -0700

    added test case for reliable multicast

--- test/tc_multicast.rb
+++ test/tc_multicast.rb
@@ -45,7 +45,6 @@ class TestMC < Test::Unit::TestCase
     mc.sync_do{ assert_equal(1, mc.mcast_done_perm.length) }
     mc.sync_do{ assert_equal("foobar", mc.mcast_done_perm.first.payload) }
     mc.sync_do{ assert_equal(1, mc2.rcv_perm.length) }
-    return
     mc.sync_do{ assert_equal(1, mc3.rcv_perm.length) }
     mc.sync_do{ assert_equal("foobar", mc2.rcv_perm.first.payload) }
 
@@ -53,4 +52,28 @@ class TestMC < Test::Unit::TestCase
     mc2.stop_bg
     mc3.stop_bg
   end
+
+  def test_reliable
+    mc = RMC.new
+    mc2 = RMC.new
+    mc3 = RMC.new
+
+    mc2.run_bg; mc3.run_bg
+    mc.add_member <+ [[mc2.ip_port], [mc3.ip_port]]
+    mc.run_bg
+
+    resps = mc.sync_callback(mc.send_mcast.tabname, [[1, 'foobar']], mc.mcast_done.tabname)
+    resps.each do |resp|
+      assert_equal(resp.ident, 1)
+    end
+    assert_equal(mc2.rcv_perm.length, 1)
+    assert_equal(mc3.rcv_perm.length, 1)
+    assert(mc2.rcv_perm.include? [1, 'foobar'])
+    assert(mc3.rcv_perm.include? [1, 'foobar'])
+
+    mc.stop_bg
+    mc2.stop_bg
+    mc3.stop_bg
+  end
+
 end
