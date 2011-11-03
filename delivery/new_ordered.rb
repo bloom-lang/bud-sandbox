3bfe5f81cac8cb65012f8a9497a349420f3d65ab
Neil Conway <nrc@cs.berkeley.edu>
2011-11-02 17:38:17 -0700

    Checkpoint work on initial ordered delivery protocol + test.
    Basic functionality seems to work.

--- delivery/new_ordered.rb
+++ delivery/new_ordered.rb
@@ -20,6 +20,7 @@ module OrderedDelivery
 
   state do
     channel :ord_chan, [:@dst, :src, :seq] => [:ident, :payload]
+    channel :ack_chan, [:@dst, :src, :seq]
 
     # Sender state
     table :send_seq, [] => [:val]
@@ -29,14 +30,14 @@ module OrderedDelivery
 
     # Receiver state
     table :recv_buf, ord_chan.schema
-    table :recv_deliver_seq, [] => [:val]
+    table :recv_deliver_seq, [:sender] => [:val]
+    scratch :new_sender_msg, ord_chan.schema
     scratch :to_deliver, ord_chan.schema
     periodic :recv_tik, 1
   end
 
   bootstrap do
     send_seq <= [[0]]
-    recv_deliver_seq <= [[0]]
   end
 
   bloom :snd do
@@ -44,25 +45,63 @@ module OrderedDelivery
       [m.dst, m.src, i, m.ident, m.payload]
     end
     to_send <= (pipe_in_ord * send_seq).pairs do |m, i|
-      [m.dst, m.src, m.seq + i, m.ident, m.payload]
+      [m.dst, m.src, m.seq + i.val + 1, m.ident, m.payload]
     end
     ord_chan <~ to_send
     send_buf <= to_send
+    stdio <~ to_send {|m| ["To send @ #{port}: #{m.inspect}"]}
+  end
+
+  bloom :on_ack do
+    stdio <~ ack_chan {|m| ["Got ack @ #{port}: #{m.inspect}"]}
+
+    # Remove all messages from the buffer that the recipient has acknowledged
+    # and report successful delivery to the client application
+    send_buf <- (send_buf * ack_chan).pairs(:dst => :src) do |m, a|
+      m if m.seq <= a.seq
+    end
+    pipe_sent <= (send_buf * ack_chan).pairs(:dst => :src) do |m, a|
+      [m.dst, m.src, m.ident, m.payload] if m.seq <= a.seq
+    end
+
+    # Resend all pending messages not contained in the acknowledgment
+    # XXX: this means resending far too much if the recipient has only missed a
+    # single message
+    ord_chan <~ (send_buf * ack_chan).pairs(:dst => :src) do |m, a|
+      m if m.seq > a.seq
+    end
   end
 
   bloom :recv do
-    # Don't bother trying to buffer messages that we've already delivered to the
-    # client application
-    recv_buf <= (ord_chan * recv_deliver_seq).pairs do |m, s|
+    stdio <~ ord_chan {|m| ["Received @ #{port}: #{m.inspect}"]}
+
+    # Don't bother trying to buffer messages that have already been delivered to
+    # the client application
+    recv_buf <= (ord_chan * recv_deliver_seq).pairs(:src => :sender) do |m, s|
       m if m.seq > s.val
     end
 
+    # If this is the first message we've seen from this sender, set the sequence
+    # number low water mark to 0
+    # XXX: no reason for this to really be non-monotonic
+    new_sender_msg <= ord_chan.notin(recv_deliver_seq, :src => :sender)
+    recv_deliver_seq <+ new_sender_msg {|m| [m.src, 0]}
+    recv_buf <= new_sender_msg
+
     # Check if we can legally deliver any messages in the buffer
-    to_deliver <= (recv_buf * recv_deliver_seq).pairs do |m, s|
+    to_deliver <= (recv_buf * recv_deliver_seq).pairs(:src => :sender) do |m, s|
       m if m.seq == (s.val + 1)
     end
     recv_buf <- to_deliver
-    recv_deliver_seq <+- to_deliver {|m| m.seq}
+    recv_deliver_seq <+- (to_deliver * recv_deliver_seq).lefts(:src => :sender) do |m|
+      [m.src, m.seq]
+    end
     pipe_out <= to_deliver {|m| [m.dst, m.src, m.ident, m.payload]}
+
+    # Periodically inform each sender of the greatest-delivered sequence number
+    # at this site
+    ack_chan <~ (recv_tik * recv_deliver_seq).rights do |s|
+      [s.sender, ip_port, s.val]
+    end
   end
 end
--- test/tc_ordered_delivery.rb
+++ test/tc_ordered_delivery.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'bud'
 require 'test/unit'
-require 'delivery/ordered_delivery'
+require 'delivery/new_ordered'
 
 class OrdAgent
   include Bud
@@ -9,14 +9,44 @@ class OrdAgent
 
   state do
     table :recv_log, pipe_out.schema
+    table :sent_log, pipe_sent.schema
   end
 
   bloom do
     recv_log <= pipe_out
+    sent_log <= pipe_sent
   end
 end
 
 class TestOrderedDelivery < Test::Unit::TestCase
   def test_basic
+    agents = (1..2).map { OrdAgent.new }
+    agents.each {|a| a.run_bg}
+
+    a, b = agents
+    recv_q = Queue.new
+    b.register_callback(:pipe_out) do
+      recv_q.push(true)
+    end
+    send_q = Queue.new
+    a.register_callback(:pipe_sent) do
+      send_q.push(true)
+    end
+
+    a.sync_do {
+      a.pipe_in <+ [[b.ip_port, a.ip_port, 1, "foo"]]
+    }
+    recv_q.pop
+
+    b.sync_do {
+      assert_equal([[b.ip_port, a.ip_port, 1, "foo"]], b.recv_log.to_a.sort)
+    }
+
+    send_q.pop
+    a.sync_do {
+      assert_equal([[b.ip_port, a.ip_port, 1, "foo"]], a.sent_log.to_a.sort)
+    }
+
+    agents.each {|a| a.stop}
   end
 end
