205fcc05e274041808a4a18466d8b9bd8515ef65
Neil Conway <nrc@cs.berkeley.edu>
2011-12-07 16:55:43 -0800

    Update VC scenario program to work with new lattice code.

--- lattices/vc_scenario.rb
+++ lattices/vc_scenario.rb
@@ -11,23 +11,23 @@ class VcAgent
     scratch :kickoff, [] => [:v]
     scratch :done, [] => [:v]
 
-    channel :chn, [:@addr, :msg, :delay, :clock]
-    table :recv_buf, [:msg, :clock, :deliver_at_time]
+    channel :chn, [:@addr, :msg, :delay] => [:clock]
+    table :recv_buf, [:msg, :deliver_at_time] => [:clock]
     table :send_buf, [:addr, :msg, :delay, :send_at_time]
     scratch :sbuf_chosen, send_buf.schema
     scratch :rbuf_chosen, recv_buf.schema
     periodic :tik, 1
 
-    lat_map :my_vc
-    lat_map :next_vc
+    lmap :my_vc
+    lmap :next_vc
   end
 
   bootstrap do
-    my_vc <= [[ip_port, MaxLattice.wrap(0)]]
+    my_vc <= [ {ip_port => Bud::MaxLattice.new(0)} ]
   end
 
   bloom do
-    stdio <~ rbuf_chosen {|r| ["#{@budtime} -- Delivering message @ #{port} (node # = #{$addr_map[ip_port]}), msg ID = #{r.msg}, msg clock = #{r.clock.inspected}, local VC = #{my_vc.inspected}"]}
+    stdio <~ rbuf_chosen {|r| ["#{@budtime} -- Delivering message @ #{port} (node # = #{$addr_map[ip_port]}), msg ID = #{r.msg}, msg clock = #{r.clock.inspect}, local VC = #{my_vc.inspect}"]}
 
     # Setup a specific messaging scenario: node 3 will (usually) receive message
     # 3 followed by message 1, violating causal order
@@ -41,7 +41,7 @@ class VcAgent
 
     # At the receiver, simulate network delay by buffering messages for the # of
     # ticks (seconds) requested by the message sender.
-    recv_buf <= chn {|r| [r.msg, r.clock, @budtime + r.delay]}
+    recv_buf <= chn {|r| [r.msg, @budtime + r.delay, r.clock]}
     rbuf_chosen <= recv_buf {|r| r if r.deliver_at_time == @budtime}
     recv_buf <- rbuf_chosen
     done <= rbuf_chosen {|r| [true] if ($addr_map[ip_port] == 2 and r.msg == 100)}
@@ -49,14 +49,14 @@ class VcAgent
     # If there are any incoming or outgoing messages, bump the local VC; merge
     # local VC with VCs of incoming messages
     next_vc <= my_vc
-    next_vc <= sbuf_chosen { [ip_port, my_vc[ip_port] + 1]}
-    next_vc <= rbuf_chosen { [ip_port, my_vc[ip_port] + 1]}
+    next_vc <= sbuf_chosen { {ip_port => my_vc.at(ip_port) + 1} }
+    next_vc <= rbuf_chosen { {ip_port => my_vc.at(ip_port) + 1} }
     next_vc <= rbuf_chosen {|c| c.clock}
     my_vc <+ next_vc
 
     # A violation of causal order has occurred if we receive a message that
     # strictly precedes our local clock
-    stdio <~ rbuf_chosen {|r| ["Message #{r.msg} violates causal order! Msg clock = #{r.clock.inspected}, local clock = #{my_vc.inspected}"] if r.clock.lt_eq(my_vc)}
+    stdio <~ rbuf_chosen {|r| ["Message #{r.msg} violates causal order! Msg clock = #{r.clock.inspect}, local clock = #{my_vc.inspect}"] if r.clock.lt_eq(my_vc)}
   end
 end
 
@@ -84,7 +84,7 @@ q.pop
 $nodes.each do |n|
   n.sync_do     # Make sure that the final next_vc => my_vc merge occurs
   n.sync_do {
-    puts "#{n.my_vc.inspected} @ #{n.ip_port}"
+    puts "#{n.my_vc.inspect} @ #{n.ip_port}"
   }
   n.stop
 end
