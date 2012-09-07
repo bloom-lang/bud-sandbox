c10b8d2ef22f58ba2f0e6675ea5f99c89ea7cdfe
Neil Conway <nrc@cs.berkeley.edu>
2012-09-07 16:05:45 -0700

    Code cleanup for voting tests.

--- test/tc_voting.rb
+++ test/tc_voting.rb
@@ -26,9 +26,7 @@ class TestVoting < Test::Unit::TestCase
   end
 
   def start_kind(kind, port)
-    t = nil
-    eval "t = #{kind}.new(@opts.merge(:port => #{port}, :tag => #{port}, :trace => false))"
-    return t
+    kind.new(@opts.merge(:port => port, :tag => port, :trace => false))
   end
 
   def start_three(one, two, three, kind)
@@ -36,8 +34,8 @@ class TestVoting < Test::Unit::TestCase
     t2 = start_kind(kind, two)
     t3 = start_kind(kind, three)
 
-    t.add_member <+ [[1, "localhost:#{two.to_s}"]]
-    t.add_member <+ [[2, "localhost:#{three.to_s}"]]
+    t.add_member <+ [[1, "localhost:#{two}"]]
+    t.add_member <+ [[2, "localhost:#{three}"]]
 
     t.run_bg
     t2.run_bg
@@ -47,12 +45,11 @@ class TestVoting < Test::Unit::TestCase
   end
 
   def test_votingpair
-    t, t2, t3 = start_three(12346, 12347, 12348, "VA")
+    t, t2, t3 = start_three(12346, 12347, 12348, VA)
 
     t.sync_do{ t.begin_vote <+ [[1, 'me for king']] }
-    t2.sync_do
-    t2.sync_do{ assert([[1,'me for king', 'localhost:12346'], [1,'me for king', '127.0.0.1:12346']].include? t2.waiting_ballots.first) }
-    t3.sync_do{ assert([[1,'me for king', 'localhost:12346'], [1,'me for king', '127.0.0.1:12346']].include? t3.waiting_ballots.first) }
+    t2.sync_do{ assert_equal([1, 'me for king', t.ip_port], t2.waiting_ballots.first) }
+    t3.sync_do{ assert_equal([1, 'me for king', t.ip_port], t3.waiting_ballots.first) }
     t.sync_do{ assert_equal([1, 'yes', 2, [nil].to_set], t.vote_cnt.first) }
     t.sync_do{ assert_equal([1, 'me for king', 'yes', [nil].to_set], t.vote_status.first) }
 
@@ -62,19 +59,17 @@ class TestVoting < Test::Unit::TestCase
   end
 
   def test_votingpair2
-    t, t2, t3 = start_three(12316, 12317, 12318, "VA2")
+    t, t2, t3 = start_three(12316, 12317, 12318, VA2)
 
     t.sync_do{ t.begin_vote <+ [[1, 'me for king']] }
     t2.sync_do
-    t2.sync_do{ assert([[1,'me for king', 'localhost:12316'], [1,'me for king', '127.0.0.1:12316']].include? t2.waiting_ballots.first) }
-    t3.sync_do{ assert([[1,'me for king', 'localhost:12316'], [1,'me for king', '127.0.0.1:12316']].include? t3.waiting_ballots.first) }
+    t2.sync_do{ assert_equal([1, 'me for king', t.ip_port], t2.waiting_ballots.first) }
+    t3.sync_do{ assert_equal([1, 'me for king', t.ip_port], t3.waiting_ballots.first) }
     t2.sync_do{ t2.cast_vote <+ [[1, "hell yes", "sir"]] }
-    t.sync_do{}
 
     t.sync_do{ assert_equal([1, 'hell yes', 1, ["sir"].to_set], t.vote_cnt.first) }
     t.sync_do{ assert_equal([1, 'me for king', 'in flight', nil], t.vote_status.first) }
     t3.sync_do{ t3.cast_vote <+ [[1, "hell yes", "madam"]] }
-    t.sync_do
 
     t.sync_do{ assert_equal([1, 'hell yes', 2, ["madam", "sir"].to_set], t.vote_cnt.first)}
     t.sync_do{ assert_equal([1, 'me for king', 'hell yes', ["madam", "sir"].to_set], t.vote_status.first) }
